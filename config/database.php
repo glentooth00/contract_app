fe617f5228139d7f10e959c5d0299567a5b13004
Glen <glendonnalpasan1@gmail.com>
2025-07-14 08:29:15 +0800

    wip

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
         ],
         'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'server' => '192.168.4.67',
+            'server' => '192.168.1.130',
             'database' => 'contract_monitoring',
             'username' => '',
             'password' => '',
