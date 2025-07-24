7406789e7acbbc26c0014feddf7e8206d2651cfd
Glen <glendonnalpasan1@gmail.com>
2025-07-24 16:07:13 +0800

    wip
    notification bell update

--- views/admin/view_header/view_header.php
+++ views/admin/view_header/view_header.php
@@ -27,18 +27,50 @@ $account_no = $getContract['account_no'];
         ?>
         <div class="d-flex align-items-center gap-2">
 
-        <a href="pending_updates.php" style="text-decoration: none;margin-left:1em;float:right">
-                <div style="position: relative; display: inline-block; margin-right: 30px;">
-                    <?php if (!empty($getLatestActivities)): ?>
-                        <span class="badge bg-danger" style="position: absolute; top: -10px; right: -10px;
-                display: inline-flex; justify-content: center; align-items: center;
-                border-radius: 50%; width: 20px; height: 20px; font-size: 12px;">
-                            <?= $getLatestActivities ?>
-                        </span>
-                    <?php endif; ?>
-                    <img width="25px" src="../../../public/images/bell.svg" alt="Activities need attention">
-                </div>
-            </a>
+         <?php if (isset($department)) { ?>
+
+                <?php switch ($department) {
+                    case 'IT': ?>
+
+                        <span class="badge p-2" style="background-color: #0d6efd;"><?= $department . ' ' . $role ?> user</span>
+
+                        <?php break;
+                    case 'ISD': ?>
+
+                        <span class="badge p-2" style="background-color: #3F7D58;"><?= $department . ' ' . $role ?> user</span>
+
+                        <?php break;
+                    case 'CITETD': ?>
+
+                        <span class="badge p-2" style="background-color: #FFB433;"><?= $department . ' ' . $role ?> user</span>
+
+                        <?php break;
+                    case 'IASD': ?>
+
+                        <span class="badge p-2" style="background-color: #EB5B00;"><?= $department . ' ' . $role ?> user</span>
+
+                        <?php break;
+                    case 'ISD-MSD': ?>
+
+                        <span class="badge p-2" style="background-color: #6A9C89;"><?= $department . ' ' . $role ?> user</span>
+
+                        <?php break;
+                    case 'BAC': ?>
+
+                        <span class="badge p-2" style="background-color: #3B6790;"><?= $department . ' ' . $role ?> user</span>
+
+                        <?php break;
+                    case '': ?>
+
+                    <?php default: ?>
+
+                <?php } ?>
+
+            <?php } else { ?>
+
+        <?php } ?>
+
+        <?php include_once 'bell.php'; ?>
 
             <!-- Comment icon with badge -->
             <div id="viewComment" class="position-relative">
