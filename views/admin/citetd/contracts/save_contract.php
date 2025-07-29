54a8c9409edbec93e0d7914a7b8f6eb0c7bdc4d9
Glen <glendonnalpasan1@gmail.com>
2025-07-29 08:56:58 +0800

    wip

--- views/admin/citetd/contracts/save_contract.php
+++ views/admin/citetd/contracts/save_contract.php
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] === POST) {
         'uploader_department' => $_POST['uploader_department'],
     ];
 
-    var_dump($data);
+    // var_dump($data);
 
 
     $saveContract = (new ContractController)->storeContract($data);
