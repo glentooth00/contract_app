86e0516acd59ff6c636df0a5c28a1e367dec9c35
Glen <glendonnalpasan1@gmail.com>
2025-08-29 11:01:11 +0800

    wip
    added suspension for power suppliers contract SHORT TERM

--- views/admin/citetd/contracts/end_suspension.php
+++ views/admin/citetd/contracts/end_suspension.php
@@ -207,8 +207,7 @@ if ($remainingDays == 0) {
 
     }
 
-
-        if ($type === SACC) {
+    if ($type === SACC) {
 
         $resumeContractData = [
             'contract_end' => $returnDate,
@@ -240,7 +239,71 @@ if ($remainingDays == 0) {
 
     }
 
-        if ($type === INFRA) {
+    if ($type === INFRA) {
+
+        $resumeContractData = [
+            'contract_end' => $returnDate,
+            'id' => $endSuspensionData['contract_id'],
+            'contract_status' => 'Active',
+        ];
+
+        // var_dump($resumeContractData);
+
+        $resumeContract = (new ContractController)->updateSuspension($resumeContractData);
+
+        if ($resumeContract) {
+
+            $id = $resumeContractData['id'];
+
+            $deleteSuspension = (new SuspensionController)->deleteSuspension($id);
+
+            if ($deleteSuspension) {
+                $_SESSION['notification'] = [
+                    'message' => "Contract successfully resumed! Remaining days: $remaining_days",
+                    'type' => 'success'
+                ];
+
+                header("Location: " . $_SERVER['HTTP_REFERER']);
+                exit;
+            }
+
+        }
+
+    }
+
+    if ($type === PSC_LONG) {
+
+        $resumeContractData = [
+            'contract_end' => $returnDate,
+            'id' => $endSuspensionData['contract_id'],
+            'contract_status' => 'Active',
+        ];
+
+        // var_dump($resumeContractData);
+
+        $resumeContract = (new ContractController)->updateSuspension($resumeContractData);
+
+        if ($resumeContract) {
+
+            $id = $resumeContractData['id'];
+
+            $deleteSuspension = (new SuspensionController)->deleteSuspension($id);
+
+            if ($deleteSuspension) {
+                $_SESSION['notification'] = [
+                    'message' => "Contract successfully resumed! Remaining days: $remaining_days",
+                    'type' => 'success'
+                ];
+
+                header("Location: " . $_SERVER['HTTP_REFERER']);
+                exit;
+            }
+
+        }
+
+    }
+
+    if ($type === PSC_SHORT) {
 
         $resumeContractData = [
             'contract_end' => $returnDate,
